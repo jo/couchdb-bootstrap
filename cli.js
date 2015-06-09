1ec03506cd7cb756333226afdbd56dd339d3cbb3
Johannes J. Schmidt <jo@die-tf.de>
2015-06-09 11:27:41 +0200

    don not parse options on the CLI

--- cli.js
+++ cli.js
@@ -4,10 +4,9 @@ var bootstrap = require('./')
 
 var url = process.argv[2]
 var source = process.argv[3]
-var options = require('minimist')(process.argv.slice(4))
 
 
-bootstrap(url, source, options, function(error, response) {
+bootstrap(url, source, function(error, response) {
   if (error) return console.error(error)
 
   console.log(JSON.stringify(response, null, '  '))
